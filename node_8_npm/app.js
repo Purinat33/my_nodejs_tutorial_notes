6618982dae0786f497772432d9f9ed1c0f121c6a
Purinat33 <Purinat33@gmail.com>
2023-01-31 13:42:03 +0700

    start learning npm

---
+++ node_8_npm/app.js
@@ -0,0 +1,23 @@
+//Sharable javascript code (with npm) are sometime called
+//Dependencies, packages, modules
+
+//npmjs.com
+//npm is already installed with node
+
+//npm == global command , comes with Node
+//npm --version
+
+//local dependency == use it only in this particular project
+//npm i <package_name>
+//////////////////////////////////////
+//global dependency == use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> 
+// (macos)
+
+
+// ***
+//package.json == manifest file (stores important info about project/package)
+//Manual approach: creating package.json in the root of the project, creating properties and etc.)
+//npm init: A step-by-step wizard, press enter to skip a skip
+//npm init -y: everything default
---
+++ node_9_npm_init/README.md
@@ -0,0 +1,21 @@
+# Pretend this is the root of the project
+
+1. Package name: default = folder name. But if you want to publish this then the name should be unique
+
+2. Version: Default (1.0.0) for now
+3. Description: default or anything is fine
+4. Entry point: Default for now
+5. Test command, git repository, keywords: go for default for now
+6. Authors:
+
+
+Yes to make package.json
+
+<hr>
+
+You can always manually change the values in the json file later
+so 
+```
+npm init -y
+```
+is the way to go
