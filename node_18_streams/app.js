88a018365e32fef6051f12b932ea1d8c956ba020
Purinat33 <Purinat33@gmail.com>
2023-02-02 11:50:23 +0700

    Streams

--- node_18_streams/app.js
+++ node_18_streams/app.js
@@ -1,1 +1,20 @@
 //Many modules has built in streams
+//For big files (readfilesync writefilesync might not be efficient for this)
+
+const {createReadStream} = require('fs');
+
+//Default chunk reading = 64kb
+// last chunk = remainder
+//highWaterMark = control size
+const stream = createReadStream('./content/big.txt', {
+    highWaterMark: 90000, 
+    encoding: 'utf-8'
+});
+
+stream.on('data', (result)=>{
+    console.log(result);
+});
+
+stream.on('error', (err)=>{
+    console.log(err);
+});
---
+++ node_18_streams/writeBigFile.js
@@ -0,0 +1,4 @@
+// const {writeFileSync} = require('fs');
+// for(let i = 0 ; i < 100000; i++){
+//     writeFileSync(`./content/big.txt`, `I am zebra of the lord worm our kingdom space time clock house on the mountain hill ${i}\n`, {flag: 'a'});
+// }
