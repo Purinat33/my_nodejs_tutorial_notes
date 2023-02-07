35356bb125449d5b357319467ef6f5264cd35fb7
Purinat33 <Purinat33@gmail.com>
2023-02-07 22:43:28 +0700

    TODO: Middleware

---
+++ node_33_middleware/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//Express middleware
+//Functions that executes during reqest and has access to req and res 
+//Middleware are pretty much used to make up expressjs
+
+
+
+app.listen(3000,()=>{
+
+})
