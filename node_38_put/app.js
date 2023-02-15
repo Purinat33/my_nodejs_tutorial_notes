e19d073e1604d980bcf93eb5526eab15705f1b1d
Purinat33 <Purinat33@gmail.com>
2023-02-15 22:40:57 +0700

    PUT

--- node_38_put/app.js
+++ node_38_put/app.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const {people} = require('./data')
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
 app.get('/api/people', (req,res)=>{
-    
+    res.status(200).json({success:true, data:people});
 })
 
-app.post('/api/people', (req,res)=>{
+app.post('/api/people/', (req,res)=>{
+    const { name } = req.body;
+    if(!name){
+        return res.status(401).json({success:false});
+    }
+
+    res.status(201).json({success:true, data: [...people, name]});
 
 });
+
+app.put('api/people/', (req,res)=>{
+    
+})
 
 app.listen(3000, (req,res)=>{
     console.log('Server is listening on port 3000...');
