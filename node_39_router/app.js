219b8d9aa2d1c6afa4d9d104938125593226bbad
Purinat33 <Purinat33@gmail.com>
2023-02-16 17:20:21 +0700

    TODO: controller

--- node_39_router/app.js
+++ node_39_router/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const {people} = require('./data')
+
+const people = require('./routes/people'); //People routers
+const auth = require('./routes/auth')
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+
+//Router
+app.use('/api/people', people);
+app.use('/login', auth);
 
 
 //We can see that the workspace is starting to get crowded
@@ -14,70 +21,11 @@ app.use(express.json());
 
 //Notice we have 2 distinct groups
 //1. login route
-app.post('/login', (req,res)=>{
 
-})
 
 
 //2. api/people routes
-app.get('/api/people', (req,res)=>{
-    res.status(200).json({success:true, data:people});
-})
 
-app.post('/api/people/', (req,res)=>{
-    const { name, id } = req.body;
-    if(!name || !id){
-        return res.status(401).json({success:false});
-    }
-
-    res.status(201).json({success:true, data: [...people, {"id":id,"name":name}]});
-
-});
-
-app.put('/api/people/:id', (req,res)=>{
-    const {id} = req.params; //Use to look for the specific item
-    const {name} = req.body; //To update when we found the ID
-    console.log(id, name);
-
-    const person = people.find((person)=>
-        person.id === Number(id)
-    )
-
-    if(!person){
-        return res
-        .status(401)
-        .json({success:false, msg:`No person with id ${id}`});
-    }
-
-    const newPeople= people.map(
-        (person)=>{
-            if(person.id===Number(id)){
-                person.name = name;
-            }
-            return person;
-        }
-    );
-    res.status(200).json({success:true, data: newPeople})
-})
-
-app.delete('/api/people/:id', (req,res)=>{
-    //Look for the person 
-    const person = people.find((person)=>
-        person.id === Number(req.params.id)
-    )
-
-    if(!person){
-        return res
-        .status(404)
-        .json({success:false, msg:`No person with id ${req.params.id}`});
-    }
-
-    const newPeople = people.filter(
-        (person)=> person.id !== Number(req.params.id)
-    )
-    return res.status(200).json({success:true, data:newPeople})
-
-}); //Setup is very familiar with PUT()
 
 
 
--- node_39_router/routes/people.js
+++ node_39_router/routes/people.js
@@ -1,1 +1,78 @@
 //For people api
+const express = require('express');
+//Instead of going app = express();
+//we make router instead
+
+const router = express.Router();
+
+
+//We will move this one later
+const {people} = require('./../data')
+
+
+
+//Remove every link
+//Since we use this with app.get(/api/people)
+//So we remove the api/people parts of everything
+//Thus we remove the complexity of it
+router.get('/', (req,res)=>{
+    res.status(200).json({success:true, data:people});
+})
+
+router.post('/', (req,res)=>{
+    const { name, id } = req.body;
+    if(!name || !id){
+        return res.status(401).json({success:false});
+    }
+
+    res.status(201).json({success:true, data: [...people, {"id":id,"name":name}]});
+
+});
+
+router.put('/:id', (req,res)=>{
+    const {id} = req.params; //Use to look for the specific item
+    const {name} = req.body; //To update when we found the ID
+    console.log(id, name);
+
+    const person = people.find((person)=>
+        person.id === Number(id)
+    )
+
+    if(!person){
+        return res
+        .status(401)
+        .json({success:false, msg:`No person with id ${id}`});
+    }
+
+    const newPeople= people.map(
+        (person)=>{
+            if(person.id===Number(id)){
+                person.name = name;
+            }
+            return person;
+        }
+    );
+    res.status(200).json({success:true, data: newPeople})
+})
+
+router.delete('/:id', (req,res)=>{
+    //Look for the person 
+    const person = people.find((person)=>
+        person.id === Number(req.params.id)
+    )
+
+    if(!person){
+        return res
+        .status(404)
+        .json({success:false, msg:`No person with id ${req.params.id}`});
+    }
+
+    const newPeople = people.filter(
+        (person)=> person.id !== Number(req.params.id)
+    )
+    return res.status(200).json({success:true, data:newPeople})
+
+}); //Setup is very familiar with PUT()
+
+
+module.exports = router;
